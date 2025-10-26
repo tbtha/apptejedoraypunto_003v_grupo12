f3a554f811b4260725d5af814f2d62fba9e440ee
tbtha <tabathagamboa9@gmail.com>
2025-10-26 01:23:09 -0300

    1 version estable

---
+++ app/src/main/java/com/vivitasol/carcasamvvm/viewmodels/WelcomeViewModel.kt
@@ -0,0 +1,15 @@
+package com.vivitasol.carcasamvvm.viewmodels
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import com.vivitasol.carcasamvvm.R
+
+class WelcomeViewModel : ViewModel() {
+    private val _titulo = MutableStateFlow("Bienvenid@s: Perfil Administradores")
+    val titulo: StateFlow<String> = _titulo
+
+
+    private val _logoResId = MutableStateFlow(R.drawable.logo_antiguo)
+    val logoResId: StateFlow<Int> = _logoResId
+}
